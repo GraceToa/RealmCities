93813498f3087626df9c21d07554ed0355207d5d
GraceToa <gracetoa@uoc.edu>
2019-05-22 19:38:52 +0200

    push files end

---
+++ RealmLoadImage/City.swift
@@ -0,0 +1,9 @@
+//
+//  City.swift
+//  RealmLoadImage
+//
+//  Created by GraceToa on 19/05/2019.
+//  Copyright © 2019 GraceToa. All rights reserved.
+//
+
+import Foundation
---
+++ RealmLoadImage/RealmService.swift
@@ -0,0 +1,9 @@
+//
+//  RealmService.swift
+//  RealmLoadImage
+//
+//  Created by GraceToa on 20/05/2019.
+//  Copyright © 2019 GraceToa. All rights reserved.
+//
+
+import Foundation
